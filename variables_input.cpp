4c1e542b22cf906e79dc03159329d6f258ad988b
m0hammad-ali <muhammad098ali.1@gmail.com>
2026-04-07 17:39:26 +0500

    Add variables and user input

---
+++ variables_input.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int age;
+    string name;
+
+    cout << "Enter your name: ";
+    cin >> name;
+
+    cout << "Enter your age: ";
+    cin >> age;
+
+    cout << "Hello " << name << ", you are " << age << " years old." << endl;
+
+    return 0;
+}
